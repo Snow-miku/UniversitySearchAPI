0492784bc45aa11a3b02a09aaba233b18271b125
Alan Zhou <4lanzhou@gmail.com>
2024-08-27 16:26:02 -0700

    add interface for service method

--- src/main/java/com/snowmiku/universitysearchapi/service/UniversityService.java
+++ src/main/java/com/snowmiku/universitysearchapi/service/UniversityService.java
@@ -1,25 +1,12 @@
 package com.snowmiku.universitysearchapi.service;
 
-import com.snowmiku.universitysearchapi.pojo.University;
+import com.snowmiku.universitysearchapi.pojo.UniversityDTO;
 import org.springframework.stereotype.Service;
-import org.springframework.web.client.RestTemplate;
+
+import java.util.List;
 
 @Service
-public class UniversityService {
-
-    private final RestTemplate restTemplate;
-
-    public UniversityService(RestTemplate restTemplate) {
-        this.restTemplate = restTemplate;
-    }
-
-    public University[] getUniversitiesByCountry(String country) {
-        String url = "http://universities.hipolabs.com/search?country=" + country;
-        return restTemplate.getForObject(url, University[].class);
-    }
-
-    public University[] getAllUniversities() {
-        String url = "http://universities.hipolabs.com/search";
-        return restTemplate.getForObject(url, University[].class);
-    }
+public interface UniversityService {
+    UniversityDTO[] getAllUniversities();
+    UniversityDTO[] getUniversitiesByCountries(List<String> countries);
 }
